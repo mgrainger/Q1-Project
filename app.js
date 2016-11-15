357e8a776a145e05ea52a9dda1e15422eac08b3c
Murph Grainger <murph.grainger@gmail.com>
2016-11-14 17:21:14 -0700

    returning all results through zip code input

--- app.js
+++ app.js
@@ -14,8 +14,6 @@ $(document).ready(function() {
         var geoComp = 'components=postal_code:' + zip;
         var geoKey = '&key=AIzaSyDrwG2vaCL_doUJ1Io8bTNrGzxT30N6SqE';
         var ajaxGeoURL = herokuPrefix + geoAPI + geoComp + geoKey;
-        var latitude = 0;
-        var longitude = 0;
         $.ajax({
             url: ajaxGeoURL,
             type: "GET",
@@ -26,7 +24,7 @@ $(document).ready(function() {
                 console.log(latitude, longitude);
                 var swingAPI = 'https://api.swingbyswing.com/v2/courses/search_by_location?';
                 var swingCoordinates = 'lat=' + latitude + '&lng=' + longitude;
-                var swingParams = '&radius=100&active_only=yes&hole_count=18&from=1';
+                var swingParams = '&radius=100&active_only=yes&hole_count=18&order_by=global_rank&from=1';
                 var swingToken = '&access_token=9a7a612e-4ccf-4deb-a2da-cde8bc46db01';
                 var ajaxSwingURL = swingAPI + swingCoordinates + swingParams + swingToken;
                 console.log(ajaxSwingURL);
@@ -35,18 +33,93 @@ $(document).ready(function() {
                     url: ajaxSwingURL,
                     type: "GET",
                     dataType: 'json',
-                    success: function getCourses(data) {
-                        var courses = data.courses;
-                        for (var i = 0; i < courses.length; i++) {
-                            console.log(courses[i].name);
-                        }
-                    }
+                    success: getCourses,
+
                 });
             }
         });
     });
 });
 
+function getCourses(data) {
+    var courses = data.courses;
+    var moreCoursesURL = data.meta.courses.next;
+    for (var i = 0; i < courses.length; i++) {
+        console.log(courses[i].name);
+    }
+    if (moreCoursesURL) {
+        $.ajax({
+            url: moreCoursesURL,
+            type: "GET",
+            dataType: 'json',
+            success: getCourses,
+
+        });
+    }
+
+
+}
+//Search by address:
+// $('form').submit(function(event) {
+//     event.preventDefault();
+//     var $cityInput = $('#city');
+//     var $stateSelect = $('#state');
+//     var zip = $zipInput.val();
+//     $zipInput.val('');
+//     var herokuPrefix = 'https://galvanize-cors-proxy.herokuapp.com/';
+//     var geoAPI = 'https://maps.googleapis.com/maps/api/geocode/json?';
+//     var geoComp = 'components=postal_code:' + zip;
+//     var geoKey = '&key=AIzaSyDrwG2vaCL_doUJ1Io8bTNrGzxT30N6SqE';
+//     var ajaxGeoURL = herokuPrefix + geoAPI + geoComp + geoKey;
+//     $.ajax({
+//         url: ajaxGeoURL,
+//         type: "GET",
+//         dataType: 'json',
+//         success: function getCoordinates(data) {
+//             latitude = data.results[0].geometry.location.lat;
+//             longitude = data.results[0].geometry.location.lng;
+//             console.log(latitude, longitude);
+//             var swingAPI = 'https://api.swingbyswing.com/v2/courses/search_by_location?';
+//             var swingCoordinates = 'lat=' + latitude + '&lng=' + longitude;
+//             var swingParams = '&radius=100&active_only=yes&hole_count=18&order_by=global_rank&from=1';
+//             var swingToken = '&access_token=9a7a612e-4ccf-4deb-a2da-cde8bc46db01';
+//             var ajaxSwingURL = swingAPI + swingCoordinates + swingParams + swingToken;
+//             console.log(ajaxSwingURL);
+//
+// //             $.ajax({
+// //                 url: ajaxSwingURL,
+// //                 type: "GET",
+// //                 dataType: 'json',
+// //                 success: function getCourses(data) {
+// //                     var courses = data.courses;
+// //                     var moreCoursesURL = data.meta.courses.next;
+// //                     for (var i = 0; i < courses.length; i++) {
+// //                         console.log(courses[i].name);
+// //                     }
+// //                 }
+// //             });
+//
+// // $.ajax({
+// //     url: ajaxSwingURL,
+// //     type: "GET",
+// //     dataType: 'json',
+// //     success: function getMoreCourses(data) {
+// //         var moreCoursesURL = data.meta.courses.next;
+// //
+// //     }
+// //
+// // });
+// }
+// });
+// });
+//
+//
+//
+//
+//
+//
+// });
+//
 
 
 
